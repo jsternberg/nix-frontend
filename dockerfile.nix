3bded4639b122798c105679d1a8e0033de3c4944
Jonathan A. Sternberg <jonathan@sternberg.sh>
2025-10-08 13:41:08 -0500

    Sample dockerfile that runs go build

--- dockerfile.nix
+++ dockerfile.nix
@@ -1,4 +1,4 @@
-# syntax=jsternberg/dockerfile-nix
+# syntax=docker.io/jsternberg/dockerfile-nix
 
 { goVersion ? null, alpineVersion ? null }:
 
@@ -7,7 +7,36 @@
     alpine.version = "3.20";
   };
 
-  targets = { lib, ... }: {
-    default = lib.llb.image "docker.io/library/ubuntu";
+  targets = { lib, ... }:
+  let
+    golangImage = lib.llb.image "docker.io/library/golang:1.25-alpine3.21";
+
+    packages = [ "./cmd/..." ];
+    buildCommand = [ "go" "build" "-o" "/out" ] ++ packages;
+    doBuild = lib.llb.run {
+      env.CGO_ENABLED = "0";
+      workdir = "/app";
+
+      mounts = {
+        "/app".input = lib.llb.local "context" {};
+        "/root/.cache/go-build".type = "cache";
+        "/go/pkg/mod".type = "cache";
+        "/out" = {};
+      };
+
+      meta.description = {
+        "llb.customname" = "go build ${builtins.concatStringsSep " " packages}";
+      };
+    } buildCommand;
+
+    buildStage = doBuild golangImage;
+  in
+  rec {
+    binaries = {
+      outPath = "${buildStage}/out";
+      meta.installPrefix = "/bin";
+    };
+
+    default = binaries;
   };
 }
